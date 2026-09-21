cc01458ea27a188a7aaa353d44671d7d1e59915c
Legrand Nicolas <n.legrand258@gmail.com>
2026-09-21 10:16:53 +0200

    add TP n°1

---
+++ TP.md
@@ -0,0 +1,11 @@
+TP n°1
+
+- Créez votre type customisé nommé IdCard, ce type devra contenir les valeurs suivantes: 
+
+    - Une chaîne de caractères comportant le nom
+    - Une chaîne de caractères comportant le prenom
+    - Une chaîne de caractères comportant la date de naissance
+    - Un nombre pour l'âge
+    - Un boolean qui indique si on est retraité
+
+Puis déclarez une variable de ce type contenant les vos infos personnelles.
--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,12 @@
-// Array of strings
-
-const tab:string[] = ["HTML", "CSS", "JS", "TS"];
-tab[0] = "Sébastien";
-
-const isFormateurSuperCool: boolean = true;
-// console.log(isFormateurSuperCool);
-
-// null && undefined
-
-let toto = null;
-let tata;
-// console.log(toto, tata, tata === toto);
+type Formation = {
+    date:string, 
+    participants:string[]
+}; 
 
 
-// number and strings 
-let value1 = 40; 
-let value2 = [40];
-
-
-// cast string to number automatically
-// console.log(value1 == value2);
-
-// // with === there's no automatic cast
-// console.log(value1 === value2);
-
-// créer un objet notation JSON
-const obj:any = {
-    formation: {
-        participants: [
-            "Sébastien", 
-            "Michaël",
-            "Perrine", 
-            "Pascal"
-        ]
-    }
+const formationTS:Formation = {
+    date: "21/09/2026", 
+    participants: ["Sébastien", "Michaël","Perrine","Pascal"]
 };
 
-
-obj.toto = "titi";
-
-// le type void est le type de retour par défaut
-// lorsqu'on choisit de ne retourner aucune valeur
-
-// le fait de ne pas exprimer explicitement le type
-// de retour s'appelle l'inférence de type (data type infering)
-function sayHello(){
-    console.log("Hello world");
-}
-
-function by2(num:number):number{
-    return num * 2;
-}
-
-let prenom = "Michaël";
-
-sayHello();
+console.log(formationTS);
