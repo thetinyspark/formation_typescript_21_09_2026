87a4af5583130245d3aac54237669b96a1b0bca3
Legrand Nicolas <n.legrand258@gmail.com>
2026-09-21 16:26:37 +0200

    add TP n°6 correction

--- src/characters/Mage.ts
+++ src/characters/Mage.ts
@@ -1,21 +1,34 @@
 import { Heroe } from "./Heroe";
 
 export class Mage extends Heroe{
+
+    protected _mana:number = 0;
+    protected _manaMax:number = 0;
+
     constructor(
         name:string = "",
         hp:number = 0,
         atk:number = 0, 
         def:number = 0 , 
-        protected _mana:number = 0
+        mana:number = 0
+        
     ){
         super(name, hp, atk, def);
+        this._mana = mana;
+        this._manaMax = mana;
     }
 
     protected _calcDmg(def: number, atk: number): number {
         // si vous voulez réutiliser la fonction de base définie 
         // sur la classe parent, alors vous pouvez le mot clé super
+
+        const manaCost = Math.round( this._manaMax  * 0.1 );
         const basicDmg = super._calcDmg(def,atk);
-        return basicDmg;
+        if( this._mana < manaCost )
+            return basicDmg;
+
+        this.mana -= manaCost;
+        return super._calcDmg(def, atk + manaCost);
     }
 
     public get mana():number{
@@ -24,5 +37,7 @@ export class Mage extends Heroe{
 
     public set mana(value:number){
         this._mana = value;
+        if( this._mana < 0 )
+            this._mana = 0;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,4 @@ const batman:Heroe = new Heroe("Batman", 900, 40, 100);
 const spiderman:Heroe = new Heroe("Peter Parker", 900, 35, 100);
 const sonic:Heroe = new Heroe("Sonic", 300, 15000, 20);
 
-console.log(merlin.mana);
-
 
