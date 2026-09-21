bf5bcdcd54823b7410644b28d52492747cc6a5f7
Legrand Nicolas <n.legrand258@gmail.com>
2026-09-21 10:08:14 +0200

    add basic types example

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 // Array of strings
+
 const tab:string[] = ["HTML", "CSS", "JS", "TS"];
-console.log(tab);
+tab[0] = "Sébastien";
 
 const isFormateurSuperCool: boolean = true;
-console.log(isFormateurSuperCool);
+// console.log(isFormateurSuperCool);
 
 // null && undefined
 
 let toto = null;
 let tata;
-console.log(toto, tata, tata === toto);
+// console.log(toto, tata, tata === toto);
 
 
 // number and strings 
@@ -18,11 +19,39 @@ let value2 = [40];
 
 
 // cast string to number automatically
-console.log(value1 == value2);
+// console.log(value1 == value2);
 
-// with === there's no automatic cast
-console.log(value1 === value2);
+// // with === there's no automatic cast
+// console.log(value1 === value2);
 
-// eval
-const obj = eval("{name: 'toto'}");
-console.log(obj);
+// créer un objet notation JSON
+const obj:any = {
+    formation: {
+        participants: [
+            "Sébastien", 
+            "Michaël",
+            "Perrine", 
+            "Pascal"
+        ]
+    }
+};
+
+
+obj.toto = "titi";
+
+// le type void est le type de retour par défaut
+// lorsqu'on choisit de ne retourner aucune valeur
+
+// le fait de ne pas exprimer explicitement le type
+// de retour s'appelle l'inférence de type (data type infering)
+function sayHello(){
+    console.log("Hello world");
+}
+
+function by2(num:number):number{
+    return num * 2;
+}
+
+let prenom = "Michaël";
+
+sayHello();
